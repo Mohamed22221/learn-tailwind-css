177d3fd3713b1c2474a30611b338ad3b949db102
Mohamed22221 <hamdymohame7@gmail.com>
2023-09-27 00:28:23 +0300

    add config colors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const homePage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline text-green">
     Hello world!
   </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      green: colors.green,
+      main: {
+        DEFAULT : "#969906",
+        light: "#67e8f9",
+        dark: "#0e7490",
+        200 : "#969906"
+      },
+    },
   },
   plugins: [],
-}
+};

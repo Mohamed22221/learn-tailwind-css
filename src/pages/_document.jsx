daafde2a03f769e630c602a4ffa7b0760496138c
Mohamed22221 <hamdymohame7@gmail.com>
2023-09-26 22:35:23 +0300

    setup tailwind

---
+++ src/pages/_document.jsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Document() {
+  return (
+    <Html lang="ar">
+      <Head>
+        <meta name="application-name" content="Michanes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+       
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
